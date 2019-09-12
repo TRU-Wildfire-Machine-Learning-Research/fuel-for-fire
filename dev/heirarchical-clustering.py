2d311ae588a20667abae8716d3315d273fc1e22b
GagandeepBajwa <32624560+GagandeepBajwa@users.noreply.github.com>
2019-09-12 16:58:30 -0700

    Starting with agglomerative Clustering

---
+++ dev/heirarchical-clustering.py
@@ -0,0 +1,10 @@
+"""
+Class Name: Heirarchical Clustering
+Class Description: Clustering an Image using Heirarchical Clustering
+
+"""
+
+from sklearn.cluster import AgglomerativeClustering
+import numpy as np
+
+X= np.array()
