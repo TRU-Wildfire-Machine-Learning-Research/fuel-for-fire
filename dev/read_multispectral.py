44cf70db26b080881907b84270e24fc747094f2a
brad <bradrcrump@gmail.com>
2019-09-18 11:54:59 -0700

    Added show and write image functions, still In Progress

--- dev/read_multispectral.py
+++ dev/read_multispectral.py
@@ -8,7 +8,7 @@ import numpy as np
 import os.path as path
 import matplotlib.pyplot as plt
 import cv2
-
+import time as t
 
 rawImagePath = "../images/raw/"
 convertImagePath = "../images/converted/"
@@ -35,8 +35,8 @@ def convertBinary():
     bands = int(characteristicValuesDict["bands"])
     lines = int(characteristicValuesDict["lines"])
     samples = int(characteristicValuesDict["samples"])
-    print(bands)
-    print(lines)
+    print("Bands", bands, "\nLines", lines, "\nSamples", samples)
+
     data = np.fromfile(binaryImage, '<f4').reshape((bands,  lines * samples))
     print("Bytes Read: " + str(data.size))
     return data, bands, lines, samples
@@ -47,21 +47,33 @@ def createRGB(data, bands, lines, samples):
     rgb = np.zeros((samples, lines, 3))
     for i in range(0, 3):
         rgb[:, :, i] = data[band_select[i], :].reshape((samples, lines))
+
     return rgb
 
 
-def writeJPG(data):
-    # need to be able to write the image
-    print(data)
+def showImage(data):
+    plt.imshow(data)
+    plt.tight_layout()
+    plt.show()
+    plt.close()
+
+
+"""
+    Currently not working, writing all black image
+"""
+
+
+def writeImage(data):
+    cv2.imwrite("../images/converted/" + str(time) + ".png", data)
 
 
 def run():
     d, b, l, s = convertBinary()
     rgb = createRGB(d, b, l, s)
-    writeJPG(rgb)
-    plt.imshow(rgb)
-    plt.tight_layout()
-    plt.show()
+    time = t.time()
+
+    # showImage(rgb)
+    # writeImage(rgb)
 
 
 run()
