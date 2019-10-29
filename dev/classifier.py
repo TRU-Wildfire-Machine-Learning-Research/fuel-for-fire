337f54323ca3af2932aaa2e8a9bd826f48adfa5c
brad <bradrcrump@gmail.com>
2019-10-29 15:26:46 -0700

    Formatting for output, normailzation, and undersampling options

--- dev/classifier.py
+++ dev/classifier.py
@@ -18,60 +18,6 @@ This function taken from
 https://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html#sphx-glr-auto-examples-model-selection-plot-confusion-matrix-py
 """
 
-
-def plot_confusion_matrix(y_true, y_pred, classes,
-                          normalize=False,
-                          title=None,
-                          cmap=plt.cm.Blues):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    """
-    if not title:
-        if normalize:
-            title = 'Normalized confusion matrix'
-        else:
-            title = 'Confusion matrix, without normalization'
-
-    # Compute confusion matrix
-    cm = confusion_matrix(y_true, y_pred)
-    # Only use the labels that appear in the data
-    # classes = classes[unique_labels(y_true, y_pred)]
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
-    else:
-        print('Confusion matrix, without normalization')
-
-    print(cm)
-
-    fig, ax = plt.subplots()
-    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
-    ax.figure.colorbar(im, ax=ax)
-    # We want to show all ticks...
-    ax.set(xticks=np.arange(cm.shape[1]),
-           yticks=np.arange(cm.shape[0]),
-           # ... and label them with the respective list entries
-           title=title,
-           ylabel='True label',
-           xlabel='Predicted label')
-
-    # Rotate the tick labels and set their alignment.
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-             rotation_mode="anchor")
-
-    # Loop over data dimensions and create text annotations.
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i in range(cm.shape[0]):
-        for j in range(cm.shape[1]):
-            ax.text(j, i, format(cm[i, j], fmt),
-                    ha="center", va="center",
-                    color="white" if cm[i, j] > thresh else "black")
-    # fig.tight_layout()
-    return ax
-
-
 """
 getData returns a list of the file names in the data folder.
 """
@@ -119,7 +65,7 @@ def populateDataFrame(rasterBin):
         'label_shrub_bool'))
 
     for raster in rasterBin:
-        print(raster)
+
         if "S2A.bin_4x.bin_sub.bin" in raster:
 
             dataset = rasterio.open(raster)
@@ -149,7 +95,7 @@ def populateDataFrame(rasterBin):
             shrub = rasterio.open(raster).read(1)
             data_frame['label_shrub_val'] = shrub.ravel()
             # yet to decode the shrub values boolean correspondance
-    print(data_frame)
+
     return data_frame
 
 
@@ -159,21 +105,26 @@ the same size as the true pixels)
 """
 
 
-def getSample(data_frame, underSample, normalize):
+def getSample(data_frame, undersample, normalize):
     # get initial true and false for water
     X_true = data_frame[data_frame['label_water_bool'] == True]
 
-    if underSample:
+    if undersample:
+
+        # Get the same number of false samples as true samples
+        # ie (balance the classes)
         X_false = data_frame[data_frame['label_water_bool']
                              == False].sample(len(X_true))
 
-        undersample_water_frames = [X_true, X_false]
+        undersample_frames = [X_true, X_false]
 
         # Concat the undersampled true and false pixels
-        X_full = pd.concat(undersample_water_frames)
+        X_full = pd.concat(undersample_frames)
         X = X_full.loc[:, : 'swir2']  # only considers the columns up to swir2
         y = X_full['label_water_bool']
 
+        print("\n\nUndersampled Classes\nTrue Samples: ", len(
+            X_true), "\nFalse Samples: ", len(X_false), "Total Length:", len(X))
         # Normalize the data
         if normalize:
             scaler = StandardScaler(copy=True, with_mean=True, with_std=True)
@@ -183,6 +134,68 @@ def getSample(data_frame, underSample, normalize):
         else:
             return X, y
     else:
+        X_false = data_frame[data_frame['label_water_bool']
+                             == False]
+        X_true_oversample = X_true.copy()
+
+        while len(X_true_oversample) < len(X_false):
+            os_l = [X_true_oversample, X_true]
+            X_true_oversample = pd.concat(os_l)
+
+        os_list = [X_true_oversample, X_false]
+        X_full = pd.concat(os_list)
+
+        X = X_full.loc[:, : 'swir2']  # only considers the columns up to swir2
+        y = X_full['label_water_bool']
+
+        print("\n\nOversampled Classes\nTrue Samples: ", len(X_true_oversample), str(len(X_true_oversample)/len(X)) + "%",
+              "\nFalse Samples: ", len(X_false),  str(len(X_false)/len(X)) + "%",  "\nTotal Length", len(X))
+        if normalize:
+            scaler = StandardScaler(copy=True, with_mean=True, with_std=True)
+            X_norm = scaler.fit_transform(X)
+
+            return X_norm, y
+        else:
+            return X, y
+
+
+def train(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, random_state=0, test_size=0.2)
+
+    sgd_classifier = SGDClassifier(random_state=42, verbose=1, warm_start=True)
+
+    print("Begin Fitting\n")
+    y_pred = sgd_classifier.fit(X_train, y_train).predict(X_test)
+
+    print("\n{:*^30}\n".format("Training complete"))
+    print("Test score: {:.2f}".format(sgd_classifier.score(X_test, y_test)))
+
+    np.set_printoptions(precision=3)
+
+    cm = confusion_matrix(y_test, y_pred)
+
+    truenegative, falsepositive, falsenegative, truepositive = confusion_matrix(
+        y_test, y_pred).ravel()
+
+    print("Confusion Matrix\n", cm)
+    print("True Negative", truenegative)  # land guessed correctly
+    print("True Positive", truepositive)  # water guessed correctly
+    print("False Negative", falsenegative)  # Land guessed as water
+    print("False Positive", falsepositive)  # Water guessed as land
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    truenegnorm = truenegative / (truenegative + falsenegative)
+    falseposnorm = falsepositive / (falsepositive + truepositive)
+    falsenegnorm = falsenegative / (falsenegative + truenegative)
+    trueposnorm = truepositive / (truepositive + falsepositive)
+
+    print("Confusion Matrix Percentages\n", cm)
+    print("True Negative", truenegnorm)  # land guessed correctly
+    print("True Positive", trueposnorm)  # water guessed correctly
+    print("False Negative", falsenegnorm)  # Land guessed as water
+    print("False Positive", falseposnorm)  # Water guessed as land
+    print("\n\n{:*^30}\n\n".format(""))
 
 
 """
@@ -210,41 +223,9 @@ if __name__ == "__main__":
     rasters = getData(dataPath)
 
     data_frame = populateDataFrame(rasters)
-    class_names = ['water', 'not-water']
-    X, y = getSample(data_frame, normalize=true)
+    print(data_frame.columns.values)
+    X_us, y_us = getSample(data_frame, undersample=True, normalize=True)
+    X_os, y_os = getSample(data_frame, undersample=False, normalize=True)
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, random_state=0, test_size=0.2)
-
-    sgd_classifier = SGDClassifier(random_state=42, verbose=1, warm_start=True)
-    y_pred = sgd_classifier.fit(X_train, y_train).predict(X_test)
-
-    print("Test score: {:.2f}".format(sgd_classifier.score(X_test, y_test)))
-
-    np.set_printoptions(precision=3)
-
-    cm = confusion_matrix(y_test, y_pred)
-    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-    truenegative, falsepositive, falsenegative, truepositive = confusion_matrix(
-        y_test, y_pred).ravel()
-
-    truenegnorm = truenegative / (truenegative + falsenegative)
-    falseposnorm = falsepositive / (falsepositive + truepositive)
-    falsenegnorm = falsenegative / (falsenegative + truenegative)
-    trueposnorm = truepositive / (truepositive + falsepositive)
-
-    print("Confusion Matrix\n", cm)
-    print("True Negative", truenegnorm)  # land guessed correctly
-    print("True Positive", trueposnorm)  # water guessed correctly
-    print("False Negative", falsenegnorm)  # Land guessed as water
-    print("False Positive", falseposnorm)  # Water guessed as land
-    """
-    # Plot non-normalized confusion matrix
-    plot_confusion_matrix(y_test, y_pred, classes=class_names,
-                          title='Confusion matrix, without normalization')
-
-    # Plot normalized confusion matrix
-    plot_confusion_matrix(y_test, y_pred, classes=class_names, normalize=True,
-                          title='Normalized confusion matrix')
-    plt.show()
-    """
+    train(X_us, y_us)
+    train(X_os, y_os)
