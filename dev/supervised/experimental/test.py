2fcefccb174a5e1d246a8e89a17b06b319c2a53a
Brad Crump <brad@breadcrumbbuilds.com>
2020-01-26 20:03:51 -0800

    Testing custom data object - pulling header and bin files for data allocation

--- dev/supervised/experimental/test.py
+++ dev/supervised/experimental/test.py
@@ -1,3 +1,8 @@
-from tensorflow.python.client import device_lib
+import os
+import numpy as np
+from Utils.Misc import *
+from Utils.Data import Data
 
-print(device_lib.list_local_devices())
+if __name__ == "__main__":
+
+    data = Data("data", "data_img", "data_bcgw")
