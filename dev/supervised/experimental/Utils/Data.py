883e358fc94f03d81098d23d866df592cd5e3a2e
Brad Crump <brad@breadcrumbbuilds.com>
2020-01-28 18:47:47 -0800

    commitMessage

--- dev/supervised/experimental/Utils/Data.py
+++ dev/supervised/experimental/Utils/Data.py
@@ -104,3 +104,29 @@ def create_batch_generator(X, y, batch_size=128, shuffle=False):
 
     for i in range(0, X.shape[0], batch_size):
         yield (X_copy[i: i+batch_size, :], y_copy[i: i+batch_size])
+
+def rescale(arr, two_percent=True):
+    arr_min = arr.min()
+    print(arr_min)
+    arr_max = arr.max()
+    print(arr_max)
+    scaled = (arr - arr_min) / (arr_max - arr_min)
+
+    if two_percent:
+        # 2%-linear stretch transformation for hi-contrast vis
+        values = copy.deepcopy(scaled)
+        values = values.reshape(np.prod(values.shape))
+        values = values.tolist()
+        values.sort()
+        npx = len(values)  # number of pixels
+        if values[-1] < values[0]:
+            print('error: failed to sort')
+            sys.exit(1)
+        v_min = values[int(math.floor(float(npx)*0.02))]
+        v_max = values[int(math.floor(float(npx)*0.98))]
+        scaled -= v_min
+        rng = v_max - v_min
+        if rng > 0.:
+            scaled /= (v_max - v_min)
+
+    return scaled
