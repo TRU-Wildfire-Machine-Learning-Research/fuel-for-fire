47643a9c4283c292c35cdba0873af3b2a69be836
Brad Crump <brad@breadcrumbbuilds.com>
2020-02-01 18:12:13 -0800

    .

--- dev/supervised/experimental/test.py
+++ dev/supervised/experimental/test.py
@@ -7,8 +7,15 @@ from Utils.Data import Data
 if __name__ == "__main__":
 
     data = Data("data", "data_img", "data_bcgw")
+    plt.imshow(data.S2.Data)
+    plt.show()
 
-    data.Label['conifer'].showplot()
+
+
+    """
+    working, keep for now
+    """
+    #data.Label['conifer'].showplot()
     # for label in data.Label.keys():
     #     yb = data.Label[label].spatial()
     #     yr = data.Label[label].spatial(binary=False)
