87c3782001762aa03e1865ac97577f12c2b4842d
Brad Crump <brad@breadcrumbbuilds.com>
2020-02-11 19:57:05 -0800

    removing unused code

--- dev/supervised/train_mlp2-50.py
+++ dev/supervised/train_mlp2-50.py
@@ -3,41 +3,49 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
-from Utils.Misc import *
-from Utils.Data import Data
-from Utils.DataTest import *
-from Utils.Model import LayersMultiLayerPerceptron2_50
+from rasterbox.Rasterbox import Rasterbox as rb
+from Utils.targets import get_bcgw_targets
+from Utils.Model import LayersMultiLayerPerceptron
+from Utils.Model import HelperFunction
 from sklearn.model_selection import train_test_split
 
 if __name__ == "__main__":
+    targets = get_bcgw_targets()
 
-    data = Data("data", "data_img", "data_bcgw")
-    X = data.S2.ravel()
-    y = data.labels_onehot()
+    data = rb("data", "data_img", "data_bcgw", targets)
+    X = data.Combined.Data()
+    y = data.Onehot.Data()
+    X = X.reshape(164410, 23)
 
+    # split and normalize data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-    mean_vals, std_val = DataTest.mean_center_normalize(X_train)
+    mean_vals, std_val = HelperFunction.mean_center_normalize(X_train)
 
     X_train_centered = (X_train - mean_vals) / std_val
     X_test_centered = (X_test - mean_vals) / std_val
 
     del X_train, X_test
 
+    print("Training Samples" )
     print(X_train_centered.shape, y_train.shape)
+    print()
+    print('Testing Samples')
     print(X_test_centered.shape, y_test.shape)
+    print()
 
-    mlpmodel = LayersMultiLayerPerceptron2_50(X_test_centered.shape[1], 9, learning_rate=0.001)
+
+    mlpmodel = LayersMultiLayerPerceptron(X_test_centered.shape[1], len(targets), learning_rate=0.001)
 
     sess = tf.Session(graph=mlpmodel.g)
-    training_costs = LayersMultiLayerPerceptron2_50.train_mlp(sess,
+    training_costs = LayersMultiLayerPerceptron.train_mlp(sess,
                                                     mlpmodel,
                                                     X_train_centered,
                                                     y_train,
                                                     num_epochs=100)
 
-    y_pred = LayersMultiLayerPerceptron2_50.predict_mlp(sess, mlpmodel, X_test_centered)
+    # y_pred = LayersMultiLayerPerceptron2_50.predict_mlp(sess, mlpmodel, X_test_centered)
 
-    print('Test Accuracy: %.2f%%' % (
-        100*np.sum(y_pred == y_test) / y_test.shape[0]
-    ))
+    # print('Test Accuracy: %.2f%%' % (
+    #     100*np.sum(y_pred == y_test) / y_test.shape[0]
+    # ))
